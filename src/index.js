10c3c42c4687d20061239255244155b10114db62
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-05-26 09:48:42 -0400

    done react use state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./components/Counter";
-
+import Counters from "./components/Component2";
 ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
